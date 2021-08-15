b61eff4b4032e8ec66bcabbeab838a3e66287512
Manoj Singh <manojsinghrana467@gmail.com>
2021-08-15 11:24:35 +0530

    User sign Up form done

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,5 +1,16 @@
+
+
 module.exports.profile = function (request, response) {
     return response.render('profile', {
         title: "User Profile",
     });
+};
+
+// Render Sign Up page
+
+module.exports.signUp = (request, response) => {
+    return response.render('user_sign_up', {
+        title: "User Sign Up",
+    })
+
 };
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const userController = require('../controller/usersController');
 
 router.get('/profile', userController.profile);
+router.get('/sign-up', userController.signUp);
 
 module.exports = router;
